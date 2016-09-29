5bc0a483be6fd9b31a5ee6259e7a66e5771d9e60
Jose Paulo <pauloneto.jose@gmail.com>
2016-09-29 13:24:27 -0300

    language

--- miscellany/constructors.js
+++ miscellany/constructors.js
@@ -2,25 +2,25 @@
 // This is another form to represent an object in java script, this way uses a
 //  constructor function to create an "raw" object and after this we can create a
 //  instance of this object with any value we want
-function Pessoa (nome, idade, gostaDe) {
-  this.nome = nome
-  this.idade = idade
-  this.gostaDe = gostaDe
-  this.comer = function (oQueComer) {
-    if (oQueComer === gostaDe) {
-      console.log(oQueComer)
+function People (name, age, likes) {
+  this.name = name
+  this.age = age
+  this.likes = likes
+  this.eat = function (whatEat) {
+    if (whatEat === likes) {
+      console.log(whatEat)
     } else {
       console.log('não gosto disso')
     }
   }
 }
 
-var pessoa1 = new Pessoa('jose', 26, 'bacon')
-var pessoa2 = new Pessoa('Steve', 41, 'linguiça')
+var jose = new People('jose', 26, 'bacon')
+var steve = new People('Steve', 41, 'linguiça')
 
-var pessoas = [pessoa1, pessoa2]
+var peoples = [jose, steve]
 
-pessoas.forEach(function (pessoa) {
-  console.log(pessoa.nome)
-  pessoa.comer('bacon')
+peoples.forEach(function (people) {
+  console.log(people.name)
+  people.eat('bacon')
 })
