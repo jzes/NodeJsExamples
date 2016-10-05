1f4ef8469fc8c461040c9086294f9687225545a7
Jose Paulo <pauloneto.jose@gmail.com>
2016-10-05 15:07:28 -0300

    thinking about objects and prototypes

--- miscellany/constructors.js
+++ miscellany/constructors.js
@@ -11,20 +11,39 @@ function People (name, age, likes) {
     if (whatEat === likes) {
       console.log(whatEat)
     } else {
-      console.log('não gosto disso')
+      console.log('I dont like this')
     }
   }
 }
 
 var jose = new People('jose', 26, 'bacon')
 var steve = new People('Steve', 41, 'linguiça')
+
+// Manipulating a object after creation of that
+
+// Adding a property in people after creation of constructor
+People.prototype.hightInCm = 170
+
+// adding a new property
 steve.from = 'India'
+
+// adding a new function for this object
+steve.play = function (instrument) {
+  console.log('Playing ' + instrument)
+}
 
 var peoples = [jose, steve]
 
-
+// Use object People
 peoples.forEach(function (people) {
-  console.log(people.name)
-  console.log(people.from)
+  console.log('new people ---------------------------------------------')
+  console.log('Name.: ' + people.name)
+  if (people.from !== undefined) {
+    console.log('From.: ' + people.from)
+  }
+  if (people.play !== undefined) {
+    console.log(people.play('Guitar'))
+  }
+  console.log(people.hightInCm)
   people.eat('bacon')
 })
