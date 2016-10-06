93ed40aba2a811457fee778b8473e9c403d6c3f8
Jose Paulo <pauloneto.jose@gmail.com>
2016-10-06 17:03:23 -0300

    aplicação web simples

---
+++ expressApp/server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+// userList-----------
+
+var userList = [{userName: ''}]
+// userList-------------
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.render('index', {users: []})
+})
+
+app.post('/', function (req, res) {
+  userList.push({userName: req.body.user})
+  res.render('index', {users: userList})
+})
+
+app.listen(3000, function () {
+  console.log('http://localhost:3000/')
+})
