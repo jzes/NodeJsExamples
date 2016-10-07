3e57b5d6248dea7b0c9a50e02f936928cfb05047
Jose Paulo <pauloneto.jose@gmail.com>
2016-10-07 16:56:14 -0300

    Creation of a simple express app

---
+++ expressApp/README.md
@@ -0,0 +1,1 @@
+##This is a simple express app, with ejs as view engine
--- expressApp/server.js
+++ expressApp/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
-// userList-----------
+// userList-------------
 
 var userList = [{userName: ''}]
 // userList-------------
